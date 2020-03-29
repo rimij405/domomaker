40e888827eb16354753e95b7812fc8d3ff8e283e
Ian Effendi <effendiian@gmail.com>
2020-03-29 00:31:18 -0400

    Fix typo

--- server/config.js
+++ server/config.js
@@ -1,9 +1,11 @@
 if(process.env.NODE_ENV === 'production'){
     const url = require('url');
 
+    console.log(process.env.REDISCLOUD_URL);
+
     const redisURL = url.parse(process.env.REDISCLOUD_URL);
     const passIndex = 1;
-    const redisPass = redisURL.auth.split('[:]')[passIndex];
+    const redisPass = redisURL.auth.split(':')[passIndex];
 
     module.exports = {
         REDISCLOUD_URL: redisURL.hostname,
